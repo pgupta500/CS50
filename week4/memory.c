87d241d3efedbfb4c5fd1a3078683e0b986cfe81
pgupta500 <108922290+pgupta500@users.noreply.github.com>
2025-11-28 17:29:35 +0530

    Clean up memory.c by removing unused code
    Removed file handling and PDF detection code.

--- week4/memory.c
+++ week4/memory.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 // Dereferencing means accessing the value stored at the memory location a pointer refers to.
 // p stores the address of x but when we dereference p using *p, we get the value of x.
 // If u ask "so why int *p = &x; and not int p = &x;", it's because p is supposed to store a memory address, not an ordinary integer.
+// Three golden rules:
+// 1. Every block of memory that you malloc() must subsequently be free()d.
+// 2. Only memory that you malloc() should be free()d.
+// 3. Do not free() a block of memory more than once.
+
 int main(void){
     int x = 4;
     int *p = &x;
@@ -11,55 +15,3 @@ int main(void){
     return 0;
 }
 
-// **Files, this is what we are learning pointers for**
-// fopen to open a file for future reading or writing
-// FILE *f = fopen("file.txt", "r");
-// fclose closes a file.
-// fread, fwrite read from and write to files respectively.
-
-
-FILE *text = fopen("file.txt", "r");
-
-//fread(buffer, 1, 4, text); text is the file name, buffer is where to store the data, 1 is the size of each element to read, 4 is the number of elements to read
-
-//DETECTING IF THE FILE IS A PDF
-
-int main(int argc, char *argv[]){
-    // ensure proper usage
-    if (argc != 2){
-        printf("Usage: ./detect file.pdf\n");
-        return 1;
-    }
-
-    // open file
-    FILE *file = fopen(argv[1], "r");
-    if (file == NULL){
-        printf("Could not open file.\n");
-        return 1;
-    }
-
-    // Create buffer for file
-
-    uint8_t buffer[4];
-
-    // Create an array of signature bytes for PDF files
-    uint8_t signature[] = {0x25, 0x50, 0x44, 0x46}; // corresponds to "%PDF"
-
-    // read first four bytes from the file
-    fread(buffer, 1, 4, file);
-
-    // check if PDF signature
-    for (int i = 0; i < 4; i++)
-    {
-        if (buffer[i] != signature[i]){
-            printf("Not a PDF file.\n");
-            fclose(file);
-            return 0;
-        }
-    }
-
-    printf("PDF file detected.\n");
-    // close file
-    fclose(file);
-    return 0;
-}
