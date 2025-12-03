01c9a0441e3acc43a96a1b49db0d801aa72908cb
pgupta500 <108922290+pgupta500@users.noreply.github.com>
2025-12-03 23:15:00 +0530

    Implement printing of linked list elements
    Added functionality to print the numbers in the linked list after adding a new node.

--- Week5/nodes.c
+++ Week5/nodes.c
@@ -27,6 +27,18 @@ int main(void){
         // This is important. It sets the next pointer of the new node to point to the current head of the list which is NULL when the loop runs for the first time, the node containing the user input from the second time and so on.
         list = n;
         // This updates the head of the list to point to the new node n, effectively adding it to the front of the list.
+        printf("%d\n", list->number);
+    }
+
+    node *ptr = NULL;
+    // This initializes a pointer ptr to traverse the linked list.
+    ptr = list;
+    while(ptr != NULL){
+        // This loop continues until ptr is NULL, meaning the end of the list has been reached.
+        printf("%d \n", ptr->number);
+        // This prints the number stored in the current node pointed to by ptr.
+        ptr = ptr->next;
+        // This moves ptr to the next node in the list.
     }
     
 }
