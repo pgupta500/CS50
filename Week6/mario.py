31e9b9a610bb3db981e6456c756088233b19dd31
pgupta500 <108922290+pgupta500@users.noreply.github.com>
2025-12-09 18:04:16 +0530

    Implement height input validation
    Added input validation for height to ensure it is between 1 and 8.

--- Week6/mario.py
+++ Week6/mario.py
@@ -1,5 +1,12 @@
-height = int(input("Height: "))
-length_space = height
+while True:
+    try:
+        height = int(input("Height: "))
+        if height>=1 and height<=8:
+            break
+    except ValueError:
+        pass
+
+length_space = height -1
 length_hash = 1
 length_hash_two = 1
 for a in range(0, height):
@@ -7,12 +14,10 @@ for a in range(0, height):
         print(" ", end="")
     for q in range(0, length_hash):
         print("#", end="")
-    print(" ", end="")
+    print("  ", end="")
     for r in range(0, length_hash_two):
         print("#", end="")
-    for p in range(0, height):
-        print(" ", end="")
-    print("\n")
+    print()
     length_space = length_space - 1
     length_hash = length_hash + 1
     length_hash_two = length_hash_two + 1
